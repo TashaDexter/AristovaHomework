809aaaa1731adc271e64d4df6b3585e0cf86b9cc
tasha_dexter <tasha_dexter@mail.ru>
2021-07-27 19:22:47 +0300

    Added BoxingUnboxing

---
+++ AristovaHomework/BoxingUnboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            int n = 4;
+            object c = n;
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Console.WriteLine("На упаковку потрачено милисекунд: " + ts.TotalMilliseconds.ToString());
+
+            stopWatch.Reset();
+
+            stopWatch.Start();            
+            object o = 53;
+            int p = (int)o;
+            stopWatch.Stop();
+            ts = stopWatch.Elapsed;
+            Console.WriteLine("На распаковку потрачено милисекунд: " + ts.TotalMilliseconds.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
