274a26aed32bd1d15f3060420c45c7b12c457c46
tasha_dexter <tasha_dexter@mail.ru>
2021-07-31 14:07:34 +0300

    Added Equivalence

---
+++ AristovaHomework/Equivalence/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Equivalence
+{
+    public class Person
+    {
+        public int PassportID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PlaceOfBirth { get; set; }
+
+        public override int GetHashCode()
+        {
+            return ShiftAndWrap(PassportID.GetHashCode(), 2) ^ FirstName.GetHashCode() + ShiftAndWrap(LastName.GetHashCode(), 2)^DateOfBirth.GetHashCode()^PlaceOfBirth.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is Person))
+                return false;
+            var person = (Person)obj;
+            return person.PassportID == PassportID
+                && person.FirstName==FirstName
+                && person.LastName==LastName
+                && person.DateOfBirth==DateOfBirth
+                && person.PlaceOfBirth==PlaceOfBirth;
+        }
+
+
+        private int ShiftAndWrap(int value, int positions)
+        {
+            positions = positions & 0x1F;
+            uint number = BitConverter.ToUInt32(BitConverter.GetBytes(value), 0);
+            uint wrapped = number >> (32 - positions);
+            return BitConverter.ToInt32(BitConverter.GetBytes((number << positions) | wrapped), 0);
+        }
+
+        //Для того, чтобы при сравнении через оператор == вызвался переопределённый метод Equals
+        //необходимо перегрузить операторы отношения == и !=
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return person1.Equals(person2);
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !person1.Equals(person2);
+        }
+
+    }
+}
---
+++ AristovaHomework/Equivalence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Equivalence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { PassportID = 124, FirstName = "Olivia", LastName="Smith", DateOfBirth = Convert.ToDateTime("25.04.98"), PlaceOfBirth = "New York" };
+            Person person2 = new Person() { PassportID = 124, FirstName = "Olivia", LastName = "Smith", DateOfBirth = Convert.ToDateTime("25.04.98"), PlaceOfBirth = "New York" };
+            
+            Console.WriteLine("Compare person1 and person2:");
+            Console.WriteLine($"Equals: {person1.Equals(person2)}");
+            Console.WriteLine($"GetHashCode: person1={person1.GetHashCode()}, person2={person2.GetHashCode()}");
+            Console.WriteLine($"Comparison with == :" + (person1 == person2));
+
+            Console.WriteLine("\nLet's change the property for the object 'person2'\n");
+            person2.PlaceOfBirth = "Tiraspol";
+
+            Console.WriteLine("Compare person1 and person2:");
+            Console.WriteLine($"Equals: {person1.Equals(person2)}");
+            Console.WriteLine($"GetHashCode: person1={person1.GetHashCode()}, person2={person2.GetHashCode()}");
+            Console.WriteLine($"Comparison with == :" + (person1 == person2));
+
+            Console.ReadKey();
+        }
+    }
+}
